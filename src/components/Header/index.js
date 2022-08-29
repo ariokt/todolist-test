b74ae253218534d437f79829120f4e453bd174f9
Ario Waskita <ariowaskita@Arios-MacBook-Pro.local>
2022-08-29 11:02:29 +0700

    Header selesai

---
+++ src/components/Header/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Header.css"
+
+const Header = () => {
+    return (
+        <div className="header w-100 ps-4 py-3">
+            <p>TO DO LIST APP</p>
+        </div>
+    )
+}
+
+export default Header
